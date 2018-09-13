4a19a7e72def769969243d321a29961caf44e248
rottabonus <feqwin@gmail.com>
2018-09-13 13:34:17 +0300

    kommentteja, bootstrap asennettu

--- src/components/List.js
+++ src/components/List.js
@@ -3,25 +3,16 @@ import Topic from './Topic'
 
 const List = ({ topics, show, subs, changeOption, sendAnswers }) => {
 
-/*if(sub.length !== 0) {
-let array = []
-Object.keys(sub).forEach(function (key) {
-	if (typeof sub[key] === 'object'){
-		array.push(sub[key])
-	}
-})
-}*/
-
 	return (
 		<div>
-		<h2>This is a List</h2>
-		<form onSubmit={sendAnswers}>
-		{topics.map((topic, i) => <Topic key={i} topic={topic} show={show} subs={subs} 
-		 changeOption={changeOption}></Topic>)}
-		 <button type="submit">send answers</button>
-		 </form>
+			<h2>This is a List</h2>
+				<form onSubmit={sendAnswers}>
+					{topics.map((topic, i) => 
+						<Topic key={i} topic={topic} show={show} subs={subs} changeOption={changeOption}></Topic>)}
+					 <button type="submit">send answers</button>
+			 	</form>
 		</div>
-		)
+	)
 }
 
 export default List
--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import TopicItem from './TopicItem'
 
-const Topic = ({ topic, show, subs, changeOption }) => {
-	if(subs.length === 0) {
-	return (
-	<div>
-	<h2 onClick={(e) => show(e, topic)}>{topic.topic}</h2>
-	</div>
-	)
+	const Topic = ({ topic, show, subs, changeOption }) => {
+		
+		if(subs.length === 0) {
+		return (
+			<div>
+				<h2 onClick={(e) => show(e, topic)}>{topic.topic}</h2>
+			</div>
+		)
 	} else {
 
-const obj = subs[0]
-const topicArray = Object.values(obj).map(topic => topic)
-console.log(topicArray)
+	const obj = subs[0]
+	const topicArray = Object.values(obj).map(topic => topic)
 
 		return (
-	<div>
-	<h2 onClick={(e) => show(e, topic)}>{topic.topic}</h2>
-	{subs[0].text !== topic.topic ? null : topicArray.map((topic, i) => <TopicItem key={i} changeOption={changeOption} topic={topic} ></TopicItem>)}
-	</div>
-	)
+			<div>
+				<h2 onClick={(e) => show(e, topic)}>{topic.topic}</h2>
+					{subs[0].text !== topic.topic ? null : 
+					topicArray.map((topic, i) => 
+					<TopicItem key={i} changeOption={changeOption} topic={topic} ></TopicItem>)}
+			</div>
+		)
 	}
 }
 
