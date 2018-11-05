d0d0107051e058f33617367fb10df777b26ad680
MikaElola <34126826+MikaElola@users.noreply.github.com>
2018-11-05 15:14:32 +0200

    progressbaria, nyt näkkyy mut tyhmän kokonen

--- src/components/ProgressBar2.js
+++ src/components/ProgressBar2.js
@@ -10,12 +10,52 @@ import prog8 from '../img/prog8.png'
 
 const ProgressBar2 = ({src}) => {
 var src2 = src + 1;
-console.log(src2)
+var kuva = "../img/prog" + src2 + '.png';
+var kuva2 = "prog" + src2;
+var imglist = [prog1, prog2, prog3, prog4, prog5, prog6, prog7, prog8];
+var progress;
+imglist.forEach(element => {
+  if ("prog" + src2 == element){
+    progress = element
+    return progress;
+  }
+});
+console.log(progress)
+
           return (
-            <div className="mikabar">
-              <img src={'../img/prog' + src2 + ".png"}></img>
-           
-           
+            <div>
+            { src == 0
+              ? <img src={prog1}></img>
+              :null
+            }
+            { src == 1
+              ? <img src={prog2}></img>
+              :null
+            }
+            { src == 2
+              ? <img src={prog3}></img>
+              :null
+            }
+            { src == 3
+              ? <img src={prog4}></img>
+              :null
+            }       
+            { src == 4
+              ? <img src={prog5}></img>
+              :null
+            }  
+            { src == 5
+              ? <img src={prog6}></img>
+              :null
+            }
+            { src == 6
+              ? <img src={prog7}></img>
+              :null
+            }
+            { src == 7
+              ? <img src={prog8}></img>
+              :null
+            }
             </div>
           );
         }
