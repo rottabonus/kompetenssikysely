9586779f4c8b12655cb04b8f5e8697aaaf51569f
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2018-11-16 09:07:36 +0200

    DB interactions through services and minor changes

--- src/services/topics.js
+++ src/services/topics.js
@@ -3,13 +3,24 @@ import database from '../fire'
 
 const getAll = async () => {
     const url = 'https://'+database.options.projectId+'.firebaseio.com/topics/.json'
-    console.log(url)
   const response = await axios.get(url)
   let allTopics = []
-  Object.values(response.data).forEach((elem) => {
+  Object.values(response.data).forEach(elem => {
     allTopics.push(elem)
   })
   return allTopics
 }
 
-export default { getAll }
+const newTopic = async (object, id) => {
+  const url = 'https://'+database.options.projectId+'.firebaseio.com/topics/'+id+'.json'
+  const response = await axios.put(url, object) //ei POST missään nimessä!!
+  return response.data
+}
+
+const removeTopic = async (object) => {
+  const url = 'https://'+database.options.projectId+'.firebaseio.com/topics/.json'
+  const response = await axios.put(url, object)
+  return response.data
+}
+
+export default { getAll, newTopic, removeTopic }
