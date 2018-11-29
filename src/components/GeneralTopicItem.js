f5c1ef149bfbedac44c683d2b472f1d234f7c5eb
rottabonus <feqwin@gmail.com>
2018-11-29 14:13:19 +0200

    fixes on bug handling same name questions in different topics

--- src/components/GeneralTopicItem.js
+++ src/components/GeneralTopicItem.js
@@ -3,12 +3,13 @@ import React from 'react'
 const GeneralTopicItem = ({ option, changeOption, parent, get }) => {
 
 		const options = Object.values(option).filter(option => typeof option === 'object')
+		const questionName = option.text + "*" + parent
 
 		return (
 			<div className="topicItemContainer">
 				<div>
 					<p className="topicItemHeader">{option.text}</p></div><fieldset>
-					{options.map((o, i) => <label className="topicItemOptions" key={i}><input type="radio" name={option.text} data-atext={option.text} data-acat={"yleinen"} data-aval={o.value} data-parent={parent} checked={get(option.text, o.value, parent)} onChange={changeOption} required /><span className="checkmark"></span>{o.text}</label>)}
+					{options.map((o, i) => <label className="topicItemOptions" key={i}><input type="radio" name={questionName} data-atext={option.text} data-acat={"yleinen"} data-aval={o.value} data-parent={parent} checked={get(option.text, o.value, parent)} onChange={changeOption} required /><span className="checkmark"></span>{o.text}</label>)}
 				</fieldset>
 			</div>)
 }
